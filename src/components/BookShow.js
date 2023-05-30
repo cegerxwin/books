9510813eb984f2a3e4fb1d6a9e7e95c0eb351f56
cegerxwin <cegerxwin@gmail.com>
2023-05-30 15:55:38 +0300

    test

---
+++ src/components/BookShow.js
@@ -0,0 +1,45 @@
+import BookEdit from './BookEdit';
+import { useState } from 'react';
+import useBooksContext from '../hooks/use-books-context';
+
+function BookShow({ book }) {
+    const [showEdit, setShowEdit] = useState(false);
+    const { deleteBookById } = useBooksContext();
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    const handleDeleteClick = () => {
+        deleteBookById(book.id);
+        
+    };
+
+    const handleSubmit = () => {
+        setShowEdit(false);
+    }
+
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit onSubmit={handleSubmit} book={book} />
+    }
+
+    return(
+        <div className="book-show">
+            <img 
+            alt="books" 
+            src={`https://picsum.photos/seed/${book.title}300/200`}/>
+            <div>{content}</div> 
+            <div className="actions">
+                <button className="edit" onClick={handleEditClick} >
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick} >
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default BookShow;
